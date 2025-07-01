9739df1dfe552baf05c2a53267754f510a77afa0
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-01 19:47:21 +0530

    code

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const app = express();
 
- const users=[{
-    name:"jhon",
-    kidneys:[{
-        healthy:false
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
     }]
- }];
+}];
  
- app.get("/",function(req, res){
-    const jhonkidneys = users[0].kidneys;
-    console.log(jhonkidneys);
- })
+app.get("/",function(req, res){
+    const johnkidneys = users[0].kidneys;
+    const numberOfKidneys = johnkidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i < numberOfKidneys; i++) {
+        if (johnkidneys[i].healthy) {
+            numberOfHealthyKidneys=numberOfHealthyKidneys + 1;
+        }
+    }
+    const numberOfUnHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnHealthyKidneys
+    })
+})
 
- app.listen(3000);
+app.listen(3000);
