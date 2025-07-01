a95dbf5afc7a0dc485e280faa939f9f99ac515ef
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-02 00:17:34 +0530

    code

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const users = [{
     }]
 }];
  
+app.use(express.json());
 app.get("/",function(req, res){
     const johnkidneys = users[0].kidneys;
     const numberOfKidneys = johnkidneys.length;
@@ -26,6 +27,7 @@ app.get("/",function(req, res){
 })
 
 app.post("/",function(req, res){
+    
     const isHealthy = req.body.isHealthy;
     users[0].kidneys.push({ healthy: isHealthy });
     res.json({
