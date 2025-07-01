1bc116c51adf4c8b57cb7105ae7e2b2adc7bf1b0
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-01 13:13:13 +0530

    users

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const express = require('express');  // Step 1: import express
-const app = express();               // Step 2: create app instance
-
-app.use(express.json());            // Optional: for JSON request handling
-
-// Example route
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-// Start server on port 4000
-app.listen(4000, () => {
-  console.log('hi there');
-});
+var users =[{
+    name:"abhu",
+    age:20,
+    email:"",
+    metadata:{
+        
+        createdAt:"2023-10-01",
+        updatedAt:"2023-10-02"
+    }
+}] 
