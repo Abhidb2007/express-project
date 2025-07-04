5323b25ede5b7a08e7da460b2e431ef56c26c608
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-04 22:02:32 +0530

    code

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ app.get("/sum",function(req, res){
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
     res.json({
-        answer: a / b
-    })
+        answer: a + b,
+    });
    
-})
+});
+app.get("/multiply",function(req, res){
+    const a = parseInt(req.query,a);
+    const b = parseInt(req.query.b);
+    res. json({
+        answer:a*b,
+    });
+});
 
-app.listen(4000);
+app.listen(3000);
 
