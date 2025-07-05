7fe2a64fdd887b206f9f326e9a77d49e4db5625e
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-05 21:32:53 +0530

    code

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const app = express();
-
-let requestCount = 0;
-
-// Middleware to count every request
-function countRequests(req, res, next) {
-    requestCount += 1;
-    console.log(`Total Requests = ${requestCount}`);
-    next();
-}
-
-app.use(countRequests); // Apply middleware to all requests
-
-// Sample route
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
-// Endpoint to get total request count
-app.get('/request-count', (req, res) => {
-    res.json({ totalRequests: requestCount });
-});
-
-app.listen(3000)
+app.post("/sum",(req, res){
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+})
+    res.json({
+        sum :a+b
+    });
+    app.listen(3000);
