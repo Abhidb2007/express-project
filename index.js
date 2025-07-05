0633e358d7d16057e2edca7fc2f188c9ddf549e8
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-05 22:20:42 +0530

    code

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-app.post("/sum",(req, res){
+app.use(app.json());
+app.post("/sum",function(req, res){
     const a = parseInt(req.body.a);
     const b = parseInt(req.body.b);
-})
     res.json({
         sum :a+b
     });
+});
     app.listen(3000);
