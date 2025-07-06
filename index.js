d98eec1b138c2073be181d628182943b2fbf33c2
Abhidb2007 <abhishekdb2210@gmail.com>
2025-07-06 15:52:36 +0530

    code

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const cors = require("cors");
+
 const app = express();
+app.use(cors({
+    hosts: []
+}))
 app.use(express.json());
-app.use(cors())
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/public/index.html");
+});
+app.use(cors());
 app.post("/sum",function(req, res){
     const a = parseInt(req.body.a);
     const b = parseInt(req.body.b);
